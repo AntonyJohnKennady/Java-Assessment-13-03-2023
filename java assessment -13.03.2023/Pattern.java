f49c7cd17ea8f3276b21c77216f14279302a8809
AntonyJohnKennady <127506261+AntonyJohnKennady@users.noreply.github.com>
2023-03-14 18:06:33 +0530

    Add files via upload

---
+++ java assessment -13.03.2023/Pattern.java
@@ -0,0 +1,17 @@
+public class Pattern {
+    public static void starPattern1()
+    {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        starPattern1();
+    }
+}
---
+++ java assessment -13.03.2023/PatternAsses.java
@@ -0,0 +1,23 @@
+public class PatternAsses {
+    public static void starPattern2()
+    {
+        int space = 0;
+        for(int i=9;i>=1;i=i-2)
+        {
+            for(int k=0;k<=space;k++)
+            {
+                System.out.print(" ");
+            }
+            space++;
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        starPattern2();
+    }
+}
